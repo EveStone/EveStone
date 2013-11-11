27b128d02bcdc3448e73dc1e4b6c6eab8f8ed229
Natsuke <andy.vanbutsele@gmail.com>
2013-11-11 17:22:32 +0100

    Interface betaV1

--- src/eveStone/vieuw/GameArea.java
+++ src/eveStone/vieuw/GameArea.java
@@ -1,29 +1,17 @@
 package eveStone.vieuw;
-import com.cloudgarden.layout.AnchorLayout;
+
 
 import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.FlowLayout;
-import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.awt.Insets;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
-import java.io.File;
 
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
-import javax.swing.border.BevelBorder;
 import javax.swing.border.LineBorder;
-import javax.swing.border.SoftBevelBorder;
 import javax.swing.SwingUtilities;
 
 
@@ -42,15 +30,35 @@ import javax.swing.SwingUtilities;
 public class GameArea extends javax.swing.JFrame {
 	private JPanel jPanelMainHaut;
 	private JPanel jPanelMainBas;
-	private JLabel jLabelCimetiere;
-	private JLabel jLabel2;
-	private JLabel jLabelCimetière;
+	private JPanel jPanelMainAdv;
+	private JLabel jLabelHerosAdv;
+	private JLabel jLabelSortHerosAdv;
+	private JLabel jLabelStructureAdv;
+	private JLabel jLabelCoqueAdv;
+	private JLabel jLabelRessourceAdv;
+	private JLabel jLabelAreaAdv;
+	private JLabel jLabelArea;
+	private JLabel jLabelRessource;
+	private JLabel jLabelSortHero;
+	private JLabel jLabelStructure;
+	private JLabel jLabelCoque;
+	private JLabel jLabelHeros;
 	private JLabel jLabel1;
+	private JLabel jLabelCarte1;
+	private JLabel jLabelCimetiere;
+	private JLabel jLabelCmetiereAdv;
+	private JPanel jPanelArea;
+	private JPanel jPanelAreaAdv;
+	private JPanel jPanel2;
+	private JPanel jPanel1;
+	private JPanel jPanelInfoAdv;
+	private JLabel jLabelDeckAdv;
+	private JLabel jLabelDeck;
+	private JPanel jPanelMain;
 	private JButton jButtonFinTour;
 	private JPanel jPanelBattleArea;
 	private JPanel jPanelHistory;
 	private JPanel jPanelDeck;
-	private JLabel jLabelDeck;
 
 	/**
 	* Auto-generated main method to display this JFrame
@@ -70,59 +78,78 @@ public class GameArea extends javax.swing.JFrame {
 		initGUI();
 	}
 	
-	public static BufferedImage resize(BufferedImage image, int width, int height){
-		BufferedImage bi = new BufferedImage(width, height, BufferedImage.TRANSLUCENT);
-		Graphics2D g2d = (Graphics2D) bi.createGraphics();
-		g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY));
-		g2d.drawImage(image, 0, 0, width, height, null);
-		g2d.dispose();
-		return bi;
-	}
-	
 	private void initGUI() {
 		try {
 			setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 			{
 				jPanelMainHaut = new JPanel();
 				GridBagLayout jPanelMainHautLayout = new GridBagLayout();
+				jPanelMainHautLayout.rowWeights = new double[] {0.1};
+				jPanelMainHautLayout.rowHeights = new int[] {7};
+				jPanelMainHautLayout.columnWeights = new double[] {0.0, 0.0, 0.1};
+				jPanelMainHautLayout.columnWidths = new int[] {106, 575, 7};
 				getContentPane().add(jPanelMainHaut, BorderLayout.NORTH);
 				jPanelMainHaut.setPreferredSize(new java.awt.Dimension(806, 92));
 				jPanelMainHaut.setBackground(new java.awt.Color(191,191,191));
-				jPanelMainHaut.setBorder(new LineBorder(new java.awt.Color(0,0,0), 1, false));
-				jPanelMainHautLayout.rowWeights = new double[] {0.1};
-				jPanelMainHautLayout.rowHeights = new int[] {7};
-				jPanelMainHautLayout.columnWeights = new double[] {0.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
-				jPanelMainHautLayout.columnWidths = new int[] {85, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+				jPanelMainHaut.setBorder(BorderFactory.createTitledBorder(""));
 				jPanelMainHaut.setLayout(jPanelMainHautLayout);
 				{
-					jLabel2 = new JLabel();
-					jPanelMainHaut.add(jLabel2, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
-					FlowLayout jLabel2Layout = new FlowLayout();
-					jLabel2.setText(" ");
-					jLabel2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("img/trou_noir.jpg")));
-					jLabel2.setPreferredSize(new java.awt.Dimension(30,40));
-					jLabel2.setLayout(null);
+					jPanelMainAdv = new JPanel();
+					GridBagLayout jPanelMainAdvLayout = new GridBagLayout();
+					jPanelMainAdvLayout.columnWidths = new int[] {7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+					jPanelMainAdvLayout.rowHeights = new int[] {7};
+					jPanelMainAdvLayout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
+					jPanelMainAdvLayout.rowWeights = new double[] {0.1};
+					jPanelMainHaut.add(jPanelMainAdv, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					jPanelMainAdv.setLayout(jPanelMainAdvLayout);
+					jPanelMainAdv.setBackground(new java.awt.Color(255,210,102));
+					{
+						jLabelCarte1 = new JLabel();
+						jPanelMainAdv.add(jLabelCarte1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelCarte1.setText("Carte1");
+						jLabelCarte1.setBorder(BorderFactory.createTitledBorder(""));
+					}
+				}
+				{
+					jLabelCmetiereAdv = new JLabel();
+					jPanelMainHaut.add(jLabelCmetiereAdv, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+					jLabelCmetiereAdv.setText("Cimetiere");
 				}
 			}
 			{
 				jPanelMainBas = new JPanel();
 				GridBagLayout jPanelMainBasLayout = new GridBagLayout();
+				jPanelMainBasLayout.rowWeights = new double[] {0.1};
+				jPanelMainBasLayout.rowHeights = new int[] {7};
+				jPanelMainBasLayout.columnWeights = new double[] {0.0, 0.0, 0.1};
+				jPanelMainBasLayout.columnWidths = new int[] {105, 580, 7};
 				getContentPane().add(jPanelMainBas, BorderLayout.SOUTH);
 				jPanelMainBas.setPreferredSize(new java.awt.Dimension(806, 74));
 				jPanelMainBas.setBackground(new java.awt.Color(191,191,191));
 				jPanelMainBas.setBorder(new LineBorder(new java.awt.Color(0,0,0), 1, false));
-				jPanelMainBasLayout.rowWeights = new double[] {0.1};
-				jPanelMainBasLayout.rowHeights = new int[] {7};
-				jPanelMainBasLayout.columnWeights = new double[] {0.0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
-				jPanelMainBasLayout.columnWidths = new int[] {86, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
 				jPanelMainBas.setLayout(jPanelMainBasLayout);
 				{
-					jLabelCimetiere = new JLabel();
-					jPanelMainBas.add(jLabelCimetiere, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+					jPanelMain = new JPanel();
+					GridBagLayout jPanelMainLayout = new GridBagLayout();
+					jPanelMainLayout.columnWidths = new int[] {7, 7, 7, 7, 7, 7, 7, 7, 7, 7};
+					jPanelMainLayout.rowHeights = new int[] {7};
+					jPanelMainLayout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1};
+					jPanelMainLayout.rowWeights = new double[] {0.1};
+					jPanelMainBas.add(jPanelMain, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					jPanelMain.setLayout(jPanelMainLayout);
+					jPanelMain.setBackground(new java.awt.Color(255,210,102));
+					{
+						jLabel1 = new JLabel();
+						jPanelMain.add(jLabel1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabel1.setText("Carte1");
+						jLabel1.setBorder(BorderFactory.createTitledBorder(""));
+					}
 				}
 				{
-					BufferedImage image=ImageIO.read(new File("img/trou_noir.jpg"));
-					BufferedImage resizedImage=resize(image, 100, 100);
+					jLabelCimetiere = new JLabel();
+					jPanelMainBas.add(jLabelCimetiere, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					jLabelCimetiere.setText("Cimetiere");
+					jLabelCimetiere.setBorder(BorderFactory.createTitledBorder(""));
 				}
 			}
 			{
@@ -144,19 +171,15 @@ public class GameArea extends javax.swing.JFrame {
 				}
 				{
 					jLabelDeck = new JLabel();
-					jPanelDeck.add(jLabelDeck, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-					jLabelDeck.setText(" ");
-					jLabelDeck.setPreferredSize(new java.awt.Dimension(70, 100));
-					jLabelDeck.setIcon(new ImageIcon(new ImageIcon("img/CarteDosV1.jpeg").getImage().getScaledInstance(jLabelDeck.getWidth(), jLabelDeck.getHeight(), Image.SCALE_AREA_AVERAGING)));
+					jPanelDeck.add(jLabelDeck, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					jLabelDeck.setText("Deck");
+					jLabelDeck.setBorder(BorderFactory.createTitledBorder(""));
 				}
 				{
-					jLabel1 = new JLabel();
-					AnchorLayout jLabel1Layout = new AnchorLayout();
-					jLabel1.setLayout(jLabel1Layout);
-					jPanelDeck.add(jLabel1, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-					jLabel1.setText(" ");
-					jLabel1.setIcon(new ImageIcon(new ImageIcon("img/CarteDosV1.jpeg").getImage().getScaledInstance(jLabelDeck.getWidth(),jLabelDeck.getHeight(),Image.SCALE_AREA_AVERAGING)));
-					jLabel1.setPreferredSize(new java.awt.Dimension(70,100));
+					jLabelDeckAdv = new JLabel();
+					jPanelDeck.add(jLabelDeckAdv, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					jLabelDeckAdv.setText("Deck Adversaire");
+					jLabelDeckAdv.setBorder(BorderFactory.createTitledBorder(""));
 				}
 			}
 			{
@@ -171,14 +194,126 @@ public class GameArea extends javax.swing.JFrame {
 				GridBagLayout jPanelBattleAreaLayout = new GridBagLayout();
 				getContentPane().add(jPanelBattleArea, BorderLayout.CENTER);
 				jPanelBattleArea.setBackground(new java.awt.Color(5,2,46));
-				jPanelBattleAreaLayout.rowWeights = new double[] {0.1, 0.1, 0.1, 0.1};
-				jPanelBattleAreaLayout.rowHeights = new int[] {7, 7, 7, 7};
-				jPanelBattleAreaLayout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1, 0.1};
-				jPanelBattleAreaLayout.columnWidths = new int[] {7, 7, 7, 7, 7};
+				jPanelBattleAreaLayout.rowWeights = new double[] {0.1, 0.1, 0.1};
+				jPanelBattleAreaLayout.rowHeights = new int[] {7, 7, 7};
+				jPanelBattleAreaLayout.columnWeights = new double[] {0.1};
+				jPanelBattleAreaLayout.columnWidths = new int[] {7};
 				jPanelBattleArea.setLayout(jPanelBattleAreaLayout);
+				{
+					jPanelInfoAdv = new JPanel();
+					GridBagLayout jPanelInfoAdvLayout = new GridBagLayout();
+					jPanelInfoAdvLayout.columnWidths = new int[] {7, 7, 7, 7, 7};
+					jPanelInfoAdvLayout.rowHeights = new int[] {7};
+					jPanelInfoAdvLayout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1, 0.1};
+					jPanelInfoAdvLayout.rowWeights = new double[] {0.1};
+					jPanelBattleArea.add(jPanelInfoAdv, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					jPanelInfoAdv.setLayout(jPanelInfoAdvLayout);
+					{
+						jLabelHerosAdv = new JLabel();
+						jPanelInfoAdv.add(jLabelHerosAdv, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelHerosAdv.setText("Heros Adverse");
+						jLabelHerosAdv.setBorder(BorderFactory.createTitledBorder(""));
+					}
+					{
+						jLabelRessourceAdv = new JLabel();
+						jPanelInfoAdv.add(jLabelRessourceAdv, new GridBagConstraints(4, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelRessourceAdv.setText("Ressource Adverse");
+						jLabelRessourceAdv.setBorder(BorderFactory.createTitledBorder(""));
+					}
+					{
+						jLabelCoqueAdv = new JLabel();
+						jPanelInfoAdv.add(jLabelCoqueAdv, new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelCoqueAdv.setText("Coque Adverse");
+						jLabelCoqueAdv.setBorder(BorderFactory.createTitledBorder(""));
+					}
+					{
+						jLabelStructureAdv = new JLabel();
+						jPanelInfoAdv.add(jLabelStructureAdv, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelStructureAdv.setText("Structure Adverse");
+						jLabelStructureAdv.setBorder(BorderFactory.createTitledBorder(""));
+					}
+					{
+						jLabelSortHerosAdv = new JLabel();
+						jPanelInfoAdv.add(jLabelSortHerosAdv, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelSortHerosAdv.setText("SortHeros Adverse");
+						jLabelSortHerosAdv.setBorder(BorderFactory.createTitledBorder(""));
+					}
+				}
+				{
+					jPanel1 = new JPanel();
+					GridBagLayout jPanel1Layout = new GridBagLayout();
+					jPanel1Layout.columnWidths = new int[] {7, 7, 7, 7, 7};
+					jPanel1Layout.rowHeights = new int[] {7};
+					jPanel1Layout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1, 0.1};
+					jPanel1Layout.rowWeights = new double[] {0.1};
+					jPanelBattleArea.add(jPanel1, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					jPanel1.setLayout(jPanel1Layout);
+					{
+						jLabelHeros = new JLabel();
+						jPanel1.add(jLabelHeros, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelHeros.setText("Heros");
+						jLabelHeros.setBorder(BorderFactory.createTitledBorder(""));
+					}
+					{
+						jLabelCoque = new JLabel();
+						jPanel1.add(jLabelCoque, new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelCoque.setText("Coque");
+						jLabelCoque.setBorder(BorderFactory.createTitledBorder(""));
+					}
+					{
+						jLabelStructure = new JLabel();
+						jPanel1.add(jLabelStructure, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelStructure.setText("Structure");
+						jLabelStructure.setBorder(BorderFactory.createTitledBorder(""));
+					}
+					{
+						jLabelSortHero = new JLabel();
+						jPanel1.add(jLabelSortHero, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelSortHero.setText("SortHeros");
+						jLabelSortHero.setBorder(BorderFactory.createTitledBorder(""));
+					}
+					{
+						jLabelRessource = new JLabel();
+						jPanel1.add(jLabelRessource, new GridBagConstraints(4, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jLabelRessource.setText("Ressource");
+						jLabelRessource.setBorder(BorderFactory.createTitledBorder(""));
+					}
+				}
+				{
+					jPanel2 = new JPanel();
+					GridBagLayout jPanel2Layout = new GridBagLayout();
+					jPanel2Layout.columnWidths = new int[] {575};
+					jPanel2Layout.rowHeights = new int[] {7, 7};
+					jPanel2Layout.columnWeights = new double[] {0.0};
+					jPanel2Layout.rowWeights = new double[] {0.1, 0.1};
+					jPanelBattleArea.add(jPanel2, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+					jPanel2.setLayout(jPanel2Layout);
+					{
+						jPanelAreaAdv = new JPanel();
+						jPanel2.add(jPanelAreaAdv, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jPanelAreaAdv.setBorder(BorderFactory.createTitledBorder(""));
+						{
+							jLabelAreaAdv = new JLabel();
+							jPanelAreaAdv.add(jLabelAreaAdv);
+							jLabelAreaAdv.setText("Zone de carte Adverse");
+							jLabelAreaAdv.setPreferredSize(new java.awt.Dimension(203, 69));
+						}
+					}
+					{
+						jPanelArea = new JPanel();
+						jPanel2.add(jPanelArea, new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+						jPanelArea.setBorder(BorderFactory.createTitledBorder(""));
+						{
+							jLabelArea = new JLabel();
+							jPanelArea.add(jLabelArea);
+							jLabelArea.setText("Zone de crate");
+							jLabelArea.setPreferredSize(new java.awt.Dimension(197, 49));
+						}
+					}
+				}
 			}
 			pack();
-			this.setSize(816, 580);
+			this.setSize(800, 600);
 		} catch (Exception e) {
 		    //add your error handling code here
 			e.printStackTrace();
