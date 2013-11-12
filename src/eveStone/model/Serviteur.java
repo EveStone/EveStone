20279dbdbd35d5d98b0691c50c48323f51ba1fd4
seidh <nicolas.dasseler@gmailcom>
2013-11-12 16:19:37 +0100

    ajout nb vie et nb degat

--- src/eveStone/model/Serviteur.java
+++ src/eveStone/model/Serviteur.java
@@ -2,9 +2,33 @@ package eveStone.model;
 
 public class Serviteur extends Carte {
 	
+	private int nbVie;
+	private int nbDommage;
+	
+	
 	public Serviteur()
 	{
 		super();
+	}
+
+
+	public int getNbVie() {
+		return nbVie;
+	}
+
+
+	public void setNbVie(int nbVie) {
+		this.nbVie = nbVie;
+	}
+
+
+	public int getNbDommage() {
+		return nbDommage;
+	}
+
+
+	public void setNbDommage(int nbDommage) {
+		this.nbDommage = nbDommage;
 	}
 
 }
