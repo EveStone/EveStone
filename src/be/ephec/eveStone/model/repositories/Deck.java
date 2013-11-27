f06726fe3aec022bc2d333a495a69d8c9725eec8
seidh <nicolas.dasseler@gmailcom>
2013-11-27 21:17:33 +0100

    ADD des deux deck COMPLET, fregate et interceptor

--- src/be/ephec/eveStone/model/repositories/Deck.java
+++ src/be/ephec/eveStone/model/repositories/Deck.java
@@ -50,26 +50,74 @@ public class Deck {
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	public LinkedList<Carte> makeListInterceptor()
+	private LinkedList<Carte> makeListInterceptor()
 	{
 		LinkedList<Carte> tmp = null;
 		tmp.add(new Serviteur("Drones d'attaque", 2, "", 2, 2));
+		tmp.add(new Serviteur("Drones d'attaque", 2, "", 2, 2));
+		tmp.add(new Serviteur("Drones de combat Elite", 5, "", 3, 5));
+		tmp.add(new Serviteur("Drones de combat Elite", 5, "", 3, 5));
+		tmp.add(new Serviteur("Drones d'attaque", 2, "", 2, 2));
+		tmp.add(new Serviteur("Drones d'attaque", 2, "", 2, 2));
+		tmp.add(new Serviteur("Drones de combat Elite", 5, "", 3, 5));
 		tmp.add(new Serviteur("Drones de combat Elite", 5, "", 3, 5));
 		tmp.add(new Dommage("Interception rapide", 5, "", "Attaque directement un serviteur ou un Hï¿½ro", 4));
 		tmp.add(new Dommage("Interception rapide", 5, "", "Attaque directement un serviteur ou un Hï¿½ro", 4));
 		tmp.add(new Serviteur("Drone de soutien",2, "", 2, 2, 1, 1));
 		tmp.add(new Serviteur("Drone de soutien",2, "", 2, 2, 1, 1));
+		tmp.add(new Protection("Drone tank et de soutien",6, "", 5, 6, 1, 1, true));
+		tmp.add(new Protection("Drone tank et de soutien",6, "", 5, 6, 1, 1, true));
+		tmp.add(new Invisible("Drone invisible",4, "", 4, 4, 2, true));
+		tmp.add(new Invisible("Drone invisible",4, "", 4, 4, 2, true));
+		tmp.add(new Buff("Bouclier augmentï¿½",2, "", "Augmente de 2 la vie d'un serviteur", 2, false));
+		tmp.add(new Buff("Bouclier augmentï¿½",2, "", "Augmente de 2 la vie d'un serviteur", 2, false));
+		tmp.add(new Serviteur("Drone lieutenant élite de combat",8, "", 6, 8, 2, 2));
+		tmp.add(new Serviteur("Drone lieutenant élite de combat",8, "", 6, 8, 2, 2));
+		tmp.add(new Serviteur("Drone de combat", 3,"", 3,3));
+		tmp.add(new Serviteur("Drone de combat", 3,"", 3,3));
+		tmp.add(new Buff("Egide des anciens",4, "", "Augmente de 4 les Pv et de 4 les dégats d'un serviteur", 4, 4));
+		tmp.add(new Buff("Egide des anciens",4, "", "Augmente de 4 les Pv et de 4 les dégats d'un serviteur", 4, 4));
+		tmp.add(new Protection("Drone tank",2, "", 2, 2, true));
+		tmp.add(new Protection("Drone tank",2, "", 2, 2, true));
+		tmp.add(new Serviteur("Drone docteur",1, "", 1, 1, 1, false));
+		tmp.add(new Serviteur("Drone docteur",1, "", 1, 1, 1, false));
+		tmp.add(new Serviteur("Drone docteur",1, "", 1, 1, 1, false));
+		tmp.add(new Serviteur("Drone docteur",1, "", 1, 1, 1, false));
 		return tmp;
 	}
-	public LinkedList<Carte> makeListFregate()
+	private LinkedList<Carte> makeListFregate()
 	{
 		LinkedList<Carte> tmp = null;
 		tmp.add(new Serviteur("Drones d'attaque", 2, "", 2, 2));
+		tmp.add(new Serviteur("Drones d'attaque", 2, "", 2, 2));
+		tmp.add(new Serviteur("Drones de combat Elite", 5, "", 3, 5));
+		tmp.add(new Serviteur("Drones de combat Elite", 5, "", 3, 5));
+		tmp.add(new Serviteur("Drones d'attaque", 2, "", 2, 2));
+		tmp.add(new Serviteur("Drones d'attaque", 2, "", 2, 2));
+		tmp.add(new Serviteur("Drones de combat Elite", 5, "", 3, 5));
 		tmp.add(new Serviteur("Drones de combat Elite", 5, "", 3, 5));
 		tmp.add(new Serviteur("Drone de soutien",2, "", 2, 2, 1, 1));
 		tmp.add(new Serviteur("Drone de soutien",2, "", 2, 2, 1, 1));
 		tmp.add(new Buff("Bouclier augmentï¿½",2, "", "Augmente de 2 la vie d'un serviteur", 2, false));
 		tmp.add(new Buff("Bouclier augmentï¿½",2, "", "Augmente de 2 la vie d'un serviteur", 2, false));
+		tmp.add(new Protection("Drone tank et de soutien",6, "", 5, 6, 1, 1, true));
+		tmp.add(new Protection("Drone tank et de soutien",6, "", 5, 6, 1, 1, true));
+		tmp.add(new Invisible("Drone invisible",4, "", 4, 4, 2, true));
+		tmp.add(new Invisible("Drone invisible",4, "", 4, 4, 2, true));
+		tmp.add(new Dommage("Attaque des tourelles", 2, "", "Attaque directement un serviteur ou un Hï¿½ro", 2));
+		tmp.add(new Dommage("Attaque des tourelles", 2, "", "Attaque directement un serviteur ou un Hï¿½ro", 2));
+		tmp.add(new Serviteur("Drone lieutenant élite de combat",8, "", 6, 8, 2, 2));
+		tmp.add(new Serviteur("Drone lieutenant élite de combat",8, "", 6, 8, 2, 2));
+		tmp.add(new Serviteur("Drone de combat", 3,"", 3,3));
+		tmp.add(new Serviteur("Drone de combat", 3,"", 3,3));
+		tmp.add(new Buff("Egide des anciens",4, "", "Augmente de 4 les Pv et de 4 les dégats d'un serviteur", 4, 4));
+		tmp.add(new Buff("Egide des anciens",4, "", "Augmente de 4 les Pv et de 4 les dégats d'un serviteur", 4, 4));
+		tmp.add(new Protection("Drone tank",2, "", 2, 2, true));
+		tmp.add(new Protection("Drone tank",2, "", 2, 2, true));
+		tmp.add(new Serviteur("Drone docteur",1, "", 1, 1, 1, false));
+		tmp.add(new Serviteur("Drone docteur",1, "", 1, 1, 1, false));
+		tmp.add(new Serviteur("Drone docteur",1, "", 1, 1, 1, false));
+		tmp.add(new Serviteur("Drone docteur",1, "", 1, 1, 1, false));
 		return tmp;
 	}
 	
