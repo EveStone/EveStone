980ad0444f60fe98fff69989da188d2061448d0e
seidh <nicolas.dasseler@gmailcom>
2013-12-03 14:18:18 +0100

    ADD du model.NET pour pouvoir envoyer les objets sur le reseau

---
+++ src/be/ephec/eveStone/model/net/ObjectSend.java
@@ -0,0 +1,41 @@
+package be.ephec.eveStone.model.net;
+
+/**
+ * Cette classe permet de créer les objet qui seront envoyer.
+ * @author nicolas, andy
+ *
+ */
+public class ObjectSend {
+	
+	private int action;//Permet de dire quelle action l'adversaire à effectuer et ainsi facilité les casting
+	private Object obj;
+	
+	/**
+	 * Constructeur de la classe ObjectSend
+	 * @param action: int ==> Donne l'action que l'adversaire a effectué
+	 * @param obj: Object ==> Donne l'objet que l'on castera en fonction de l'action effectué.
+	 */
+	public ObjectSend(int action, Object obj) {
+		this.action = action;
+		this.obj = obj;
+	}
+
+	public int getAction() {
+		return action;
+	}
+
+	public void setAction(int action) {
+		this.action = action;
+	}
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+	
+	
+
+}
